4442ad926a4b519c56c5962f8842e4e569e157ff
Odylane <cnobyn@gmail.com>
2020-09-17 15:35:47 +0200

    update UserCreateDto and UserServiceImpl

--- src/main/java/fr/formation/internalevents/services/UserServiceImpl.java
+++ src/main/java/fr/formation/internalevents/services/UserServiceImpl.java
@@ -1,10 +1,10 @@
-package fr.formation.internalevents.service;
+package fr.formation.internalevents.services;
 
 import org.springframework.stereotype.Service;
 
-import fr.formation.internalevents.business.User;
-import fr.formation.internalevents.dto.UserCreateDto;
-import fr.formation.internalevents.repository.UserRepository;
+import fr.formation.internalevents.dtos.UserCreateDto;
+import fr.formation.internalevents.entities.User;
+import fr.formation.internalevents.repositories.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -22,8 +22,8 @@ public class UserServiceImpl implements UserService {
     	user.setUsername(dto.getUsername());
     	user.setFirstname(dto.getFirstname());
     	user.setLastname(dto.getLastname());
+    	user.setEmail(dto.getEmail());
     	user.setPassword(dto.getPassword());
     	repo.save(user);
-    }
-    	
+    }	
 }
