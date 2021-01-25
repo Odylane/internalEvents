20e9f26ff69288b77dfa31640f9b33c1cd4dfb54
Odylane <cnobyn@gmail.com>
2021-01-25 22:37:22 +0100

    Top navbar in progress

---
+++ internal-events-front/src/components/TopNavbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-banner two-line>
+    <v-avatar
+      slot="icon"
+      size="40"
+    >
+      <v-img
+      src="@/assets/logo.png">
+      </v-img>
+    </v-avatar>
+
+    <v-toolbar-title>Internal Events</v-toolbar-title>
+
+  </v-banner>
+</template>
+
+<script>
+export default {
+    
+
+
+}
+</script>
+
+<style>
+
+</style>
