4719e6e5a7a2521464a2180dd0ab57b73426dfb7
Odylane <cnobyn@gmail.com>
2021-01-25 22:34:08 +0100

    footer added

--- internal-events-front/src/App.vue
+++ internal-events-front/src/App.vue
@@ -1,14 +1,32 @@
 <template>
-  <v-app>
-    <router-view></router-view>
+
+  <v-app id="inspire">
+    
+    <v-app-bar app color="indigo" dark>
+      <TopNavbar/>
+      
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+
+    <v-footer color="grey" app>
+      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+    
   </v-app>
+
 </template>
 
 <script>
+import TopNavbar from "@/components/TopNavbar.vue";
 
 export default {
-  name: 'App',
-
+  name: "App",
+  components: {
+    TopNavbar,
+  },
   data: () => ({
     //
   }),
