085b3422256bc56945067155b28eaa61ee427f11
Odylane <cnobyn@gmail.com>
2021-03-11 19:53:09 +0100

    update navbar

--- internal-events-front/src/App.vue
+++ internal-events-front/src/App.vue
@@ -1,34 +1,104 @@
 <template>
-
-  <v-app id="inspire">
-    
+  <v-app>
     <v-app-bar app color="indigo" dark>
-      <TopNavbar/>
-      
-    </v-app-bar>
+      <v-icon left>
+        mdi-alpha-l-box
+      </v-icon>
 
+      <v-toolbar-title
+        class="text-uppercase"
+        style="cursor: pointer"
+        @click="$router.push('/events')"
+        >Internal Events</v-toolbar-title
+      >
+      <v-spacer></v-spacer>
+      <v-list color="indigo">
+        <v-list-item>
+          <v-btn
+            class="button"
+            small
+            v-if="authData.token"
+            to="/topics"
+            tag="ul"
+            >Admin
+          </v-btn>
+          <v-menu offset-y v-if="authData.token">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn class="button" small white--text v-bind="attrs" v-on="on">
+                Gérer mes événements
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="(item, index) in items"
+                :key="index"
+                :to="item.route"
+              >
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+          <v-btn class="button"
+            small
+            v-if="authData.token"
+            @click.prevent="logoutEmployee"
+            tag="ul"
+          >
+            se déconnecter
+          </v-btn>
+
+          <v-btn
+            small
+            v-if="authData.token"
+            color="indigo"
+            to="/profile"
+            tag="ul"
+            ><v-icon>
+              mdi-account-circle
+            </v-icon></v-btn
+          >
+        </v-list-item>
+      </v-list>
+    </v-app-bar>
     <v-main>
-      <router-view />
+      <router-view></router-view>
     </v-main>
 
     <v-footer color="grey" app>
       <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
-    
   </v-app>
-
 </template>
 
 <script>
-import TopNavbar from "@/components/TopNavbar.vue";
+import { mapState, mapActions } from "vuex";
 
 export default {
   name: "App",
-  components: {
-    TopNavbar,
-  },
+
   data: () => ({
-    //
+    items: [
+      { title: "Créer un événement", route: "/events/create" },
+    ],
   }),
+
+  computed: {
+    ...mapState("auth", ["authData"]),
+  },
+  methods: {
+    ...mapActions("auth", ["logout"]),
+    logoutEmployee() {
+      this.logout();
+      this.$router.push("/logout");
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.button {
+  margin-right: 10px;
+  background-color: #3F51B5 !important;
+  
+}
+</style>
