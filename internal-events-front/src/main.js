99ac9a97296f0ac80ef8c288cd6a0d3ddc0edc7d
Odylane <cnobyn@gmail.com>
2021-03-11 21:11:10 +0100

    baseURL

--- internal-events-front/src/main.js
+++ internal-events-front/src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import axios from "axios";
 
+//baseURL: A relative URL that acts as a prefix to all requests
 axios.defaults.baseURL = "http://localhost:8085/api/";
 
 export default axios;
